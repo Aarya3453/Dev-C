50fd82ac8b10d9fa3a78658b4039ed7521123969
Aarya3453 <141324591+Aarya3453@users.noreply.github.com>
2023-08-14 03:01:21 -0700

    Add files via upload

---
+++ mydetails.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+
+{
+	printf("\n *****My detalis*****\n");
+	printf("\n Name    : Patel Aryan");
+	printf("\n BOD     : 02/01/2004");
+	printf("\n Age     : 19 years");
+	printf("\n Address : C/302 Goga palace");
+	
+	return 0;
+}
---
+++ subtaction.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a,b,c;
+	
+	printf("\n Enter the value of a :");
+	scanf("%d",&a);
+	
+	printf("\n Enter the value of b :");
+	scanf("%d",&b);
+	
+	c=a-b;
+	
+	printf("\n Subtraction of a & b is :%d",c);
+	
+	return 0;
+}
